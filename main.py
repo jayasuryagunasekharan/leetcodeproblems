4315b479baf860a233d1f78345fc1080140ad22c
Jayasurya Gunasekharan <jayasurya.guna@gmail.com>
2023-06-18 01:58:31 -0500

    217. Contains Duplicate

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+def contains_duplicate(nums):
+    hash_set = set()
 
+    for num in nums:
+        if num in hash_set:
+            return True
+        else:
+            hash_set.add(num)
+    return False
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    value = contains_duplicate([1,2,3,4])
+    print(value)
